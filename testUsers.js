8a58389468a2196edb8c8f81e83372fc1624e8d6
ewadkins <ewadkins83@gmail.com>
2015-09-04 19:17:42 -0400

    Updated testUsers to include profilepic

--- testUsers.js
+++ testUsers.js
@@ -1,11 +1,11 @@
 var bcrypt = require('bcrypt');
 
 var userData = [
-                new UserData('vontell', 'password', 'Aaron', 'Vontell', 'Bitch', 'vontell@mit.edu', '8608050050', '371', 2, 'http://avontell.com', 'Bristol', 'CT', 'USA'),
-                new UserData('ewadkins', 'password', 'Eric', 'Wadkins', null, 'ewadkins@mit.edu', '6178395035', '371', 2, null, 'Winthrop', 'MA', 'USA')
+                new UserData('vontell', 'password', 'Aaron', 'Vontell', 'Bitch', 'vontell@mit.edu', '8608050050', '371', 2, 'http://avontell.com', 'Bristol', 'CT', 'USA', '/images/profilepic/vontell.jpg'),
+                new UserData('ewadkins', 'password', 'Eric', 'Wadkins', null, 'ewadkins@mit.edu', '6178395035', '371', 2, null, 'Winthrop', 'MA', 'USA', null)
                 ];
 
-function UserData(username, password, firstName, lastName, nickname, email, phone, room, year, homepage, city, state, country) {
+function UserData(username, password, firstName, lastName, nickname, email, phone, room, year, homepage, city, state, country, profilePic) {
 	var salt = bcrypt.genSaltSync(10);
     var hash = bcrypt.hashSync(password, salt);
 	this.username = username;
@@ -21,6 +21,7 @@ function UserData(username, password, firstName, lastName, nickname, email, phon
 	this.city = city;
 	this.state = state;
 	this.country = country;
+	this.profilePic = profilePic;
 }
 
 module.exports = {
