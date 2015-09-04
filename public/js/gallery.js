83dcdd9384ddaa1fd36236129f0dd11bebad2258
Aaron Vontell <vontell@mit.edu>
2015-09-04 19:13:39 -0400

    Added profile pics

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -3,7 +3,7 @@ $(function() {
 	$.get('/data/gallery/', function(gallery) {
 		for (var i = 0; i < gallery.length; i++) {
 			//$('#dankmemes').append('<img style="padding:1%;width:20%;height:20%" src="/memes/' + dankmemes[i] + '"></img>');
-            $('#gallery').append('<div class="col-sm-6 col-md-4"><div class="thumbnail"><a href="' + gallery[i] + '"><img src="' + gallery[i] + '" alt="Photo alt"/></a></div></div>');
+            $('#gallery').append('<div style="text-align: center;" class="col-sm-6 col-md-4"><div style="margin-top: 16px;"><a href="' + gallery[i] + '"><img style="height: 300px; max-width: 375px;" src="' + gallery[i] + '" alt="Photo alt"/></a></div></div>');
 		}
 	});
 });
--- public/js/userPage.js
+++ public/js/userPage.js
@@ -4,6 +4,8 @@ $(function() {
 	$.get('/data/users/' + user, function(data) {
 		$('#fullName').html(data.firstName + ' ' + (data.nickname ? '"' + data.nickname + '" ' : '') + data.lastName);
         $('#username').html(data.username);
+        var profileSrc = data.profilePic ? data.profilePic : '/images/profileDefault.jpg';
+        $('#userPicture').append('<img class="img-circle hvr-grow-rotate" src="' + profileSrc + '" style="width: 300px; height: 300px; margin-bottom: 32px;"></img>');
         if(data.email){ $('#userData').append('<div>Email: <a href="mailto:' + data.email + '">' + data.email + '</a></div>'); }
         if(data.phone){ $('#userData').append('<div>Phone: <a href="tel:' + data.phone + '">' + data.phone + '</a></div>'); }
         if(data.room) { $('#userData').append('<div>Room: ' + data.room + '</div>'); }
