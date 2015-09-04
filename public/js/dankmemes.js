fb964384636eff95f039ff9fc26e46e3f79b6c00
Aaron Vontell <vontell@mit.edu>
2015-09-04 00:17:15 -0400

    Implementing bootstrap

--- public/js/dankmemes.js
+++ public/js/dankmemes.js
@@ -2,7 +2,8 @@
 $(function() {
 	$.get('/data/dankmemes/', function(dankmemes) {
 		for (var i = 0; i < dankmemes.length; i++) {
-			$('#dankmemes').append('<img style="padding:1%;width:20%;height:20%" src="/memes/' + dankmemes[i] + '"></img>');
+			//$('#dankmemes').append('<img style="padding:1%;width:20%;height:20%" src="/memes/' + dankmemes[i] + '"></img>');
+            $('#dankmemes').append('<div class="col-sm-6 col-md-4"><div class="thumbnail"><img src="/memes/' + dankmemes[i] + '" alt="Photo alt"</div></div>');
 		}
 	});
 });
