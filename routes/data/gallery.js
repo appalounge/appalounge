7ada831cd5da1d05cd16e05c3aebb31d347b89d8
ewadkins <ewadkins83@gmail.com>
2015-09-11 00:14:03 -0400

    Changes

--- routes/data/gallery.js
+++ routes/data/gallery.js
@@ -8,6 +8,12 @@ module.exports = function(db) {
 
 	router.get('/', function(req, res, next) {
 		var pics = [];
+        if (!fs.existsSync('./public/images/gallery/public')) {
+            fs.mkdirSync('./public/images/gallery/public');
+        }
+        if (!fs.existsSync('./public/images/gallery/private')) {
+            fs.mkdirSync('./public/images/gallery/private');
+        }
 		var contents = fs.readdirSync('./public/images/gallery/public');
 		for (var i = 0; i < contents.length; i++) {
 			if (contents[i].match(/^.*.jpg$/)) {
