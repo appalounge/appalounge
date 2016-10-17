0edfc9743682efd408b0453c96f5ffd47f237d2c
ewadkins <ewadkins83@gmail.com>
2016-10-16 23:13:21 -0400

    Updating to run on heroku

--- addAppaUsers.js
+++ addAppaUsers.js
@@ -1,15 +1,18 @@
+module.exports = function() {
+    
+    var config = require('./config');
+    var users = require('./users');
+    var userList = config.users;
 
-var config = require('./config');
-var users = require('./users');
-var userList = config.users;
-
-var i = 0;
-next();
-function next() {
-	if (i < userList.length) {
-		users.add(userList[i], function() {
-			i++;
-			next();
-		});
-	}
+    var i = 0;
+    next();
+    function next() {
+        if (i < userList.length) {
+            users.add(userList[i], function() {
+                i++;
+                next();
+            });
+        }
+    }
+    
 }
